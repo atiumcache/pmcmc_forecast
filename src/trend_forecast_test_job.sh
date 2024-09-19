9ddcaf03ffd627e524b838bd8d5467d0ad572acc
atiumcache <aattilio@pm.me>
2024-09-19 16:36:07 -0700

    update job script

--- src/trend_forecast_test_job.sh
+++ src/trend_forecast_test_job.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --mincpus=64
 #SBATCH --time=12:00:00
-#SBATCH --chdir=/projects/math_cheny/pmcmc_forecast/
+#SBATCH --chdir=/projects/math_cheny/pmcmc_tests_andrew/trend_forecast_test
 
 # added echo statements for debugging
 
@@ -17,7 +17,11 @@ echo -e "\nJob started at: $(date)\n"
 module load anaconda3/2024.02
 python3 -m ensurepip
 python3 -m pip install -r ./requirements.txt
+python3 -m pip uninstall numpy
+python3 -m pip install --upgrade numexpr bottleneck
+python3 -m pip install numpy<2
 echo -e "\nInstalled Python packages\n"
+
 
 echo -e "\nRunning the Python script... \n"
 echo -e "\nPython script started at: $(date)\n"
